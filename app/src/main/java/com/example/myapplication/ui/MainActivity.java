46c3d951fd14bb12aff85d87a77fe0a41473653e
Liza-dev <lizashah28@gmail.com>
2021-01-02 11:14:58 -0500

    Set data using MVVM

--- app/src/main/java/com/example/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.myapplication.ui;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -15,14 +15,17 @@ import com.example.myapplication.ui.adapter.CommitListAdapter;
 import com.example.myapplication.ui.base.BaseActivity;
 import com.example.myapplication.viewmodel.MainViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity<MainViewModel> {
     ActivityMainBinding binding;
+    MainViewModel mainViewModel;
+    private CommitListAdapter adapter;
+
     @NonNull
     @Override
     protected MainViewModel createViewModel() {
+        //MainViewModelFactory factory = new MainViewModelFactory(CommitModel.class);
         return null;
     }
 
@@ -30,20 +33,27 @@ public class MainActivity extends BaseActivity<MainViewModel> {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
+        mainViewModel = ViewModelProviders.of(this, getDefaultViewModelProviderFactory()).get(MainViewModel.class);
+        mainViewModel.loadCommitLocal();
+        mainViewModel.getCommit().observe(this, new CommitObserver());
         setupRecyclerView();
+
     }
 
     private void setupRecyclerView() {
-        RecyclerView recyclerView = binding.commitList; // In xml we have given id rv_movie_list to RecyclerView
         LinearLayoutManager layoutManager = new LinearLayoutManager(this); // you can use getContext() instead of "this"
-        recyclerView.setLayoutManager(layoutManager);
+        binding.commitList.setLayoutManager(layoutManager);
+        adapter = new CommitListAdapter(MainActivity.this);
+        binding.commitList.setAdapter(adapter);
 
-        List<CommitModel> list = new ArrayList<>();
-        list.add(new CommitModel("EndGame", "Aveneger endgame is awesome movie"));
-        list.add(new CommitModel("Infinity wars", "Aveneger infinity wars is super awesome movie"));
-        // ...
-        CommitListAdapter adapter = new CommitListAdapter(MainActivity.this);
-        adapter.setItems(list);
-        recyclerView.setAdapter(adapter);
+    }
+
+    private class CommitObserver implements Observer<List<CommitModel>> {
+        @Override
+        public void onChanged(List<CommitModel> commitModels) {
+            if (commitModels == null) return;
+            adapter.setItems(commitModels);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.java
+++ app/src/main/java/com/example/myapplication/viewmodel/MainViewModel.java
@@ -5,22 +5,44 @@ import androidx.lifecycle.MutableLiveData;
 import com.example.myapplication.model.CommitModel;
 import com.example.myapplication.ui.base.BaseViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainViewModel extends BaseViewModel {
-
-
-    private MutableLiveData<CommitModel> commitModelMutableLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<CommitModel>> commitModelMutableLiveData = new MutableLiveData<>();
     private CommitModel commitModel;
 
-    MainViewModel(CommitModel commitModel) {
-        this.commitModel = commitModel;
-    }
+//    public MainViewModel(CommitModel commitModel) {
+//        this.commitModel = commitModel;
+//    }
 
-    void loadCommitData() {
-        commitModelMutableLiveData.postValue(commitModel);
-    }
-
-    MutableLiveData<CommitModel> getCommit() {
+  public MutableLiveData<List<CommitModel>> getCommit() {
         return commitModelMutableLiveData;
+    }
+
+    public void loadCommitNetworkData() {
+        //call api
+        setMovies(createLocalList());
+    }
+
+    public void loadCommitLocal() {
+        //load local data
+        setMovies(createLocalList());
+    }
+
+    private void setMovies(List<CommitModel> localList) {
+        this.commitModelMutableLiveData.postValue(localList);
+    }
+
+    private List<CommitModel> createLocalList() {
+        String name = "Person";
+        String message = "initial Commit";
+
+        List<CommitModel> commitModels = new ArrayList<>();
+        commitModels.add(new CommitModel(name,message));
+        commitModels.add(new CommitModel(name,message));
+
+        return commitModels;
     }
 
 }
